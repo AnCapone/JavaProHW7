3536504c8970b708d6378131954680dfa5fe457b
AnCapone <okai.kot44@gmail.com>
2024-05-03 20:26:49 +0300

    Delete hardcode

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,6 @@ import coffee.order.CoffeeOrderBoard;
 public class Main {
     public static void main(String[] args) {
 
-        CoffeeOrderBoard coffeeOrders = new CoffeeOrderBoard();
-        coffeeOrders.add("Bob");
-        coffeeOrders.add("Charlie");
-        coffeeOrders.add("David");
-        coffeeOrders.add("Eve");
-        coffeeOrders.add("Frank");
-        coffeeOrders.add("Grace");
-        coffeeOrders.draw();
-        System.out.println();
-        coffeeOrders.deliver();
-        coffeeOrders.add("Heidi");
-        coffeeOrders.draw();
-        System.out.println();
-        coffeeOrders.deliver(3);
-        coffeeOrders.add("Ivan");
-        coffeeOrders.deliver();
-        coffeeOrders.deliver(5);
-        coffeeOrders.deliver(8);
-        coffeeOrders.deliver(9);
-        coffeeOrders.draw();
 
     }
 }
